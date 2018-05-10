5fe059755e563a261054fe87581c43f64fd08011
Li Kai <hello+github@lik.ai>
2018-05-10 11:13:46 +0800

    Update readme with complete instructions

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
-export const gitHubAppId = 10823;
+export const gitHubAppId = process.env.GITHUB_APP_ID || 10823;
+export const githubWebhookSecret = process.env.GITHUB_WEBHOOK_SECRET;
 export const cottonBranch = 'cotton/upgrade';
